2dfd5be7644dbb5c95c3a0d8038243d30214c3f1
barcca22 <dudova.barbora90@gmail.com>
2026-07-03 15:03:43 +0200

    Delete cteni_api.py

--- cteni_api.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-
-response = requests.get("https://catfact.ninja/fact")
-data = response.json()
-data.pop("length")
-with open("catfact.json", "w", encoding="utf-8") as file:
-    json.dump(data, file)
-
